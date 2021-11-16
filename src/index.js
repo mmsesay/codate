862269c2a10d09ae06ffd41ecb6b12670427ebb7
mmsesay <maej_01@hotmail.com>
2021-11-16 12:21:31 +0000

    Add react-router-dom package Wrapped the App compnent in the BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
